4a34f34fa1d42d60cf27dff17f0b9b7b961427ef
Hernan Vignolo <hernan.vignolo@emilabs.ai>
2024-09-17 11:40:53 -0300

    feat(infra): add airbyte source clients DB

--- infra/airbyte.tf
+++ infra/airbyte.tf
@@ -12,3 +12,25 @@ resource "helm_release" "airbyte" {
     file("${path.module}/airbyte-values.yml")
   ]
 }
+
+resource "airbyte_source_postgres" "clients_db" {
+  configuration = {
+    database = "clients"
+    host     = "clients-db"
+    username = "clients"
+    password = "clients"
+    port     = 5432
+    schemas  = ["public"]
+    replication_method = {
+      scan_changes_with_user_defined_cursor = {}
+    }
+    ssl_mode = {
+      disable = {}
+    }
+    tunnel_method = {
+      no_tunnel = {}
+    }
+  }
+  workspace_id = "071d7739-bd76-42f8-a2be-79f4a8c70cf9"
+  name         = "Clients"
+}
